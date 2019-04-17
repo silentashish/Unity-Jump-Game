e48518e1bd78707797d3fb60ad8f79e1c38ef26f
gashish <ashish.gm74@gmail.com>
2019-04-17 20:46:07 +0545

    intitial set up

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGame : MonoBehaviour {
+
+	public Rigidbody2D rd;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		rd=GetComponent<Rigidbody2D>();
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		rd.velocity = new Vector2 (1, rd.velocity.y);
+
+		if (Input.GetMouseButtonDown (0)) {
+			rd.velocity = new Vector2 (rd.velocity.x, 10);
+		}
+
+
+
+
+
+
+
+
+	}
+}
